7e7deb3423c0a4084ca8a696651d4968d6c5e69e
Alex Gray <alex@mrgray.com>
2013-10-21 06:42:41 -0400

    hmmm

--- _local/_index.php
+++
@@ -1,8 +0,0 @@
-<html>
-<frameset rows="100%">
-<frame src="<?php echo $_SERVER['FRAMEREDIRECTURL'] ?>">
-</frameset>
-<noframes>
-<body>Please follow <a href="<?php echo $_SERVER['FRAMEREDIRECTURL'] ?>">link</a>.</body>
-</noframes>
-</html>
